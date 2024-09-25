c29865ff316219ad7202e5cbcdbf1f469a9b090d
W0198745JC <w0198745@campus.nscc.ca>
2024-09-25 16:14:31 -0300

    basic pseudo code done

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -3,7 +3,14 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
+# this is the Weekly loan calculator
+# input the amount of loan (variable)
+#input the interest rate (%)(variable)
+#input the number of years (variable)
+# the calculations 
+#i = interest rate (variable) / 5200
+# weekly payment = i/1 - (1+i)**(-52*years) * loan amount  (This took me AGES to figure out the ** for power of)
+#your weekly payment will be ${0:.2f}
 
 
 
