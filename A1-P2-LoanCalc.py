085646dc03aa42b085273227a4a5578628175213
W0198745JC <w0198745@campus.nscc.ca>
2024-09-25 16:23:08 -0300

    added input variables

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -4,9 +4,13 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 # this is the Weekly loan calculator
+    print("Weekly Loan Calculator")
 # input the amount of loan (variable)
+    loanAmount=input("Enter the amount of loan: ")
 #input the interest rate (%)(variable)
+    interestRate= input("Enter the interest rate (%): ")
 #input the number of years (variable)
+    NumYears=input("Enter the number of years: ")
 # the calculations 
 #i = interest rate (variable) / 5200
 # weekly payment = i/1 - (1+i)**(-52*years) * loan amount  (This took me AGES to figure out the ** for power of)
